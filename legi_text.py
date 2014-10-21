0fd32366e5f7d484cde8696c0d177ab5b5fb5fa1
mnsd-12gbm <mnsd12gbm@gmail.com>
2014-10-21 16:02:53 -0400

    add observation columns for committee, date, etc, send to xcel document

--- legi_text.py
+++ legi_text.py
@@ -1,11 +1,19 @@
+"""
+Get data regarding NYC City Council Legislation
+"""
 import mechanize
 import re
 from bs4 import BeautifulSoup
 import urllib2
 import requests
+import collections
+import xlwt
+
+book = xlwt.Workbook(encoding='utf-8', style_compression = 0)
+sheet = book.add_sheet('Legi', cell_overwrite_ok = True)  
+row=-1
 
 for items in ['obesity']:
-
     url = r'http://legistar.council.nyc.gov/Legislation.aspx'
     request = mechanize.Request(url)
     response = mechanize.urlopen(request)
@@ -23,16 +31,34 @@ for items in ['obesity']:
             if 'LegislationDetail' in legislation:
             	url_stem = 'http://legistar.council.nyc.gov/'
                 url2 = "%s%s" % (url_stem, legislation)
-                print url2
-                
+
                 request2 = requests.get(url2)
                 soup2 = BeautifulSoup(request2.content)
                 
-                legi_date = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblName2"})
-                print legi_date[0].text
+                legislation_title = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblName2"})
+                legislation_date = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblOnAgenda2"})
+                legislation_committee = soup2.find_all("a",{"id":"ctl00_ContentPlaceHolder1_hypInControlOf2"})
+                legislation_text = soup2.find_all("span",{"class":"st1"})                                
 
-                legi_text = soup2.find_all("span",{"class":"st1"})
-                for item in legi_text:
-                    print item.text
+                legi_url, title, date, committee, text = ([] for i in range(5))
+                row = row + 1 
+
+                legi_url = url2                
+                for item in legislation_title:
+                    title.append(item.text)
+                for item in legislation_date:
+                    date.append(item.text)
+                for item in legislation_committee:
+                    committee.append(item.text)
+                for item in legislation_text:
+                    text.append(' '+item.text)
+
+                sheet.write (row, 0, legi_url)
+                sheet.write (row, 1, title)
+                sheet.write (row, 2, date)
+                sheet.write (row, 3, committee)
+                sheet.write (row, 4, text)
         except:
             pass
+
+book.save("legislation_results.xls")

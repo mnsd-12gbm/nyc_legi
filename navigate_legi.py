4c7c034a0df0cb0ef2e60a71239c92edcea2af2c
mnsd-12gbm <mnsd12gbm@gmail.com>
2014-10-30 21:06:29 -0400

    extend and combine, functional, needs improvements

--- navigate_legi.py
+++ navigate_legi.py
@@ -1,31 +1,88 @@
 #!/usr/bin/python
 import re
 import time
+import xlwt
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from pyvirtualdisplay import Display
+from selenium.webdriver.support.ui import Select
+import requests
 
-display = Display(visible=0, size=(1024, 768))
-display.start()
+book = xlwt.Workbook(encoding='utf-8', style_compression = 0)
+sheet = book.add_sheet('Legi', cell_overwrite_ok = True)  
+row=-1
 
 profile = webdriver.FirefoxProfile()
 profile.set_preference("general.useragent.override","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0")
 driver=webdriver.Firefox(profile)
 
 driver.get('http://legistar.council.nyc.gov/Legislation.aspx')
-inputElement = driver.find_element_by_name("ctl00$ContentPlaceHolder1$txtSearch")
-inputElement.send_keys('street')
+driver.find_element_by_link_text('Advanced search >>>').click()
 
-inputButton = driver.find_element_by_name("ctl00$ContentPlaceHolder1$btnSearch")
-inputButton.click()
+dropdown = driver.find_element_by_name('ctl00$ContentPlaceHolder1$lstMax')
+dropdown.click()
 
-html = driver.page_source.encode('utf-8')
-soup = BeautifulSoup(html)
+driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstMax_DropDown').click()
+for val in driver.find_elements_by_class_name('rcbItem'):
+    if val.text == 'All':
+        val.click()
 
-#print soup
+dropdown = driver.find_element_by_name('ctl00$ContentPlaceHolder1$lstYearsAdvanced')
+dropdown.click()
 
-driver.implicitly_wait(10)
+driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstYearsAdvanced_DropDown').click()
+for val in driver.find_elements_by_class_name('rcbItem'):
+    if val.text == 'All Years':
+        val.click()
 
-for
-driver.find_element_by_link_text('2').click()
+SearchButton = driver.find_element_by_name("ctl00$ContentPlaceHolder1$btnSearch2")
+SearchButton.click()
+
+for i in range(2,4):
+    driver.implicitly_wait(10)
+    hrefs = driver.find_elements_by_xpath("//*[@href]")
+    for j in hrefs:
+        legislation = j.get_attribute("href")
+        try:
+            if 'LegislationDetail' in legislation:
+                url2 = "%s" % (legislation)
+                request2 = requests.get(url2)
+                soup2 = BeautifulSoup(request2.content)
+                type = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblType2"})
+                status = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblStatus2"})
+                print url2
+
+                if ((type[0].text == "Resolution" or 
+                    type[0].text == "Introduction") and 
+                    (status[0].text == "Adopted")):
+
+                    legislation_title = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblName2"})
+                    legislation_date = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblOnAgenda2"})
+                    legislation_committee = soup2.find_all("a",{"id":"ctl00_ContentPlaceHolder1_hypInControlOf2"})
+                    legislation_text = soup2.find_all("span",{"class":"st1"})                                
+
+                    legi_url, title, date, committee, text = ([] for i in range(5))
+                    row = row + 1 
+
+                    legi_url = url2                
+                    for item in legislation_title:
+                        title.append(item.text)
+                    for item in legislation_date:
+                        date.append(item.text)
+                    for item in legislation_committee:
+                        committee.append(item.text)
+                    for item in legislation_text:
+                        text.append(' '+item.text)
+
+                    legi = [legi_url,title,date,committee,text]
+                    for column, var_observ in enumerate(legi):
+                        sheet.write (row, column, var_observ)
+                    time.sleep(1)
+        except:
+            pass
+
+    next_page = "%d" % (i)
+    driver.find_element_by_link_text(next_page).click()
+    driver.implicitly_wait(10)
+
+book.save("legislation_data.xls")

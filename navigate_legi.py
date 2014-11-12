4ad49d42d03ed76667032269c0931292217179a9
mnsd-12gbm <mnsd12gbm@gmail.com>
2014-11-12 12:43:43 -0500

    distorted to get output, in separate sets of 1000s

--- navigate_legi.py
+++ navigate_legi.py
@@ -1,59 +1,107 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import re
 import time
 import xlwt
 import requests
 import sys
+import csv
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
+writer = csv.writer(open("legi_data.csv", "wb"))
+"""
+with open('legi_data.csv', 'w') as file:
+    writer = csv.writer(file, delimiter = ',', lineterminator = '\n',)
+
 print sys.getdefaultencoding()
 book = xlwt.Workbook(encoding='utf-8', style_compression = 0)
-sheet = book.add_sheet('Legi', cell_overwrite_ok = True)  
+sheet = book.add_sheet('Legi', cell_overwrite_ok = False)  
+"""
+
 row=-1
+for i in range(1,21):
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("general.useragent.override","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0")
-driver=webdriver.Firefox(profile)
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("general.useragent.override","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0")
+    driver=webdriver.Firefox(profile)
 
-driver.get('http://legistar.council.nyc.gov/Legislation.aspx')
-driver.find_element_by_link_text('Advanced search >>>').click()
+    driver.get('http://legistar.council.nyc.gov/Legislation.aspx')
+    driver.find_element_by_link_text('Advanced search >>>').click()
+    
+    inputElement1 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_txtOnAgenda1_dateInput")
+    inputElement1.clear()
+    inputElement1.send_keys('01/01/2002')
+    inputElement2 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_txtOnAgenda2_dateInput")
+    inputElement2.clear()
+    #'12/31/2013'
+    #'05/12/2010'
+    #'02/15/2006'
+    #'03/13/2002'
+    inputElement2.send_keys('03/13/2002')
+    driver.find_element_by_id("ctl00_ContentPlaceHolder1_radOnAgenda_3").click()
 
-# Select to remove limit on searched legislative items in drop-down menu
-dropdown = driver.find_element_by_name('ctl00$ContentPlaceHolder1$lstMax')
-dropdown.click()
+    # Select "Introduction" as document type in drop-down menu
+    dropdown = driver.find_element_by_name('ctl00$ContentPlaceHolder1$lstType')
+    dropdown.click()
 
-driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstMax_DropDown').click()
-for val in driver.find_elements_by_class_name('rcbItem'):
-    if val.text == 'All':
-        val.click()
+    driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstType_DropDown').click()
+    for val in driver.find_elements_by_class_name('rcbItem'):
+        if val.text == 'Introduction':
+            val.click()
 
-# Select "All Years" in drop-down menu
-dropdown = driver.find_element_by_name('ctl00$ContentPlaceHolder1$lstYearsAdvanced')
-dropdown.click()
+    # Select to remove limit on searched legislative items in drop-down menu
+    dropdown = driver.find_element_by_name('ctl00$ContentPlaceHolder1$lstMax')
+    dropdown.click()
 
-driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstYearsAdvanced_DropDown').click()
-for val in driver.find_elements_by_class_name('rcbItem'):
-    if val.text == 'All Years':
-        val.click()
+    driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstMax_DropDown').click()
+    for val in driver.find_elements_by_class_name('rcbItem'):
+        if val.text == 'All':
+            val.click()
 
-# Select "Introduction" as document type in drop-down menu
-dropdown = driver.find_element_by_name('ctl00$ContentPlaceHolder1$lstType')
-dropdown.click()
+    # Select "All Years" in drop-down menu
+    dropdown = driver.find_element_by_name('ctl00$ContentPlaceHolder1$lstYearsAdvanced')
+    dropdown.click()
 
-driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstType_DropDown').click()
-for val in driver.find_elements_by_class_name('rcbItem'):
-    if val.text == 'Introduction':
-        val.click()
+    driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstYearsAdvanced_DropDown').click()
+    for val in driver.find_elements_by_class_name('rcbItem'):
+        if val.text == 'All Years':
+            val.click()
 
-# Submit the search
-SearchButton = driver.find_element_by_name("ctl00$ContentPlaceHolder1$btnSearch2")
-SearchButton.click()
+    # Submit the search
+    SearchButton = driver.find_element_by_name("ctl00$ContentPlaceHolder1$btnSearch2")
+    SearchButton.click()
 
-driver.find_element_by_link_text('8').click()
-for i in range(9,11):
+    next_page = "%d" % (i)
+    if i in range(2,11): 
+
+        driver.find_element_by_link_text(next_page).click()
+        print next_page
+    else:
+        pass
+    if i in range(11,21):
+
+        driver.find_element_by_link_text('...').click()
+        driver.implicitly_wait(10)
+
+        inputElement1 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_txtOnAgenda1_dateInput")
+        inputElement2 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_txtOnAgenda2_dateInput")
+        if inputElement1 == '':
+            inputElement1.send_keys('01/01/2002')
+        if inputElement2 == '':
+            inputElement2.send_keys('12/31/2013')
+        driver.find_element_by_id("ctl00_ContentPlaceHolder1_radOnAgenda_3").click()
+
+        driver.find_element_by_id("ctl00_ContentPlaceHolder1_radOnAgenda_3").click()
+        driver.find_element_by_link_text(next_page).click()
+        print next_page
+    else:
+        pass
+
+    driver.implicitly_wait(1)
     hrefs = driver.find_elements_by_xpath("//*[@href]")
     lg_list = []
 
@@ -73,57 +121,57 @@ for i in range(9,11):
                 soup2 = BeautifulSoup(url2_html,'html.parser')
 
                 agenda_date = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblOnAgenda2"})
-                a_date = []
+                passed_date = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblPassed2"})
+                legislation_number = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblFile2"})
+                legislation_status = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblStatus2"})
+                legislation_name = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblName2"})
+                legislation_title = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblTitle2"})
+                legislation_committee = soup2.find_all("a",{"id":"ctl00_ContentPlaceHolder1_hypInControlOf2"})
+                legislation_text = soup2.find_all("span",{"class":"st1"})                                
+                passed_date = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblPassed2"})  
+
+                title, l_name, legi_num, status,  a_date, p_date, committee, l_text, = ([] for i in range(8))
+                row = row + 1 
+
+                for item in legislation_title:
+                    item = item(text=True)
+                    title.append(''.join(item) if item else 'NA')
+                for item in legislation_name:
+                    item = item(text=True)
+                    l_name.append(''.join(item) if item else 'NA')
+                for item in legislation_number:
+                    item = item(text=True)
+                    legi_num.append(''.join(item) if item else 'NA')
+                for item in legislation_status:
+                    item = item(text=True)
+                    status.append(''.join(item) if item else 'NA')
                 for item in agenda_date:
                     item = item(text=True)
                     a_date.append(''.join(item) if item else 'NA')
+                for item in passed_date:
+                    item = item(text=True)
+                    p_date.append(''.join(item) if item else 'NA')
+                for item in legislation_committee:
+                    item = item(text=True)
+                    committee.append(''.join(item) if item else 'NA')
+                for item in legislation_text:
+                    item = item(text=True)
+                    l_text.append(' '.join(item) if item else ' ')
+                #lg_text.append(''.join([i if ord(i) < 128 else ' ' for i in l_text]))
+                #lg_text = re.split('Be it enacted by the Council as follows:' , l_text)
+                #lg_text = removeNonAscii(l_text)
 
-                bloomberg_years = list(range(2002,2013))
-                if a_date[-4:] in bloomberg_years:    
-                    passed_date = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblPassed2"})
-                    legislation_number = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblFile2"})
-                    legislation_status = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblStatus2"})
-                    legislation_name = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblName2"})
-                    legislation_title = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblTitle2"})
-                    legislation_committee = soup2.find_all("a",{"id":"ctl00_ContentPlaceHolder1_hypInControlOf2"})
-                    legislation_text = soup2.find_all("span",{"class":"st1"})                                
-                    passed_date = soup2.find_all("span",{"id":"ctl00_ContentPlaceHolder1_lblPassed2"})  
+                legi = [i, url2,title,l_name,legi_num,status,a_date,p_date,committee,l_text]
+                print a_date
+                print p_date
+                print row
 
-                    title, name, legi_num, status,  p_date, committee, l_text = ([] for i in range(7))
-                    row = row + 1 
-
-                    for item in legislation_title:
-                        item = item(text=True)
-                        title.append(''.join(item) if item else 'NA')
-                    for item in legislation_name:
-                        item = item(text=True)
-                        name.append(''.join(item) if item else 'NA')
-                    for item in legislation_number:
-                        item = item(text=True)
-                        legi_num.append(''.join(item) if item else 'NA')
-                    for item in legislation_status:
-                        item = item(text=True)
-                        status.append(''.join(item) if item else 'NA')
-                    for item in passed_date:
-                        item = item(text=True)
-                        p_date.append(''.join(item) if item else 'NA')
-                    for item in legislation_committee:
-                        item = item(text=True)
-                        committee.append(''.join(item) if item else 'NA')
-                    for item in legislation_text:
-                        item = item(text=True)
-                        l_text.append(' '.join(item) if item else ' ')
-                    lg_text = re.split('Be it enacted by the Council as follows:' , l_text)
-
-                    legi = [url2,title,name,legi_num,status,a_date,p_date,committee,lg_text[1]]
-                    for column, var_observ in enumerate(legi):
-                        sheet.write (row, column, var_observ)
-                    book.save("legis_data.xls")
+                writer.writerow([i,url2,title,l_name,legi_num,status,a_date,p_date,committee,l_text])
+                """
+                for column, var_observ in enumerate(legi):
+                    sheet.write (row, column, var_observ)
+                book.save("legis_data.xls")
+                """
         except:
             pass
 
-    next_page = "%d" % (i)
-    driver.find_element_by_link_text(next_page).click()
-    driver.implicitly_wait(1)
-
-book.save("legis_data.xls")
